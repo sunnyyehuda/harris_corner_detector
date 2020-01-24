2a996eca2f5c2dd31c80b9f7af9b157e0a263b74
sunnyyehuda <sunnyyehuda@gmail.com>
2020-01-24 17:02:43 +0200

    Add files via upload
    improved the detector from Harris response to min eigenvalue.
    and set the threshold to 300

--- harris_corner_detector.py
+++ harris_corner_detector.py
@@ -4,12 +4,12 @@ from scipy import signal
 import numpy as np
 
 
-def harris_corner_detection(image, master_slave):
+def harris_corner_detection(image):
     corners = []
     image_gray = rgb2gray(image)
     image_smooth = signal.convolve2d(image_gray, np.array([[-1, -1, -1],
-                                                      [-1, 32, -1],
-                                                      [-1, -1, -1]]), mode='same')
+                                                           [-1, 32, -1],
+                                                           [-1, -1, -1]]), mode='same')
     # Sobel_X filter
     Ix = signal.convolve2d(image_smooth, np.array([[-1, 0, 1],
                                                    [-2, 0, 2],
@@ -21,17 +21,16 @@ def harris_corner_detection(image, master_slave):
     Ixx = Ix ** 2
     Ixy = Iy * Ix
     Iyy = Iy ** 2
-    k = 0.03
-    for y in range(int((image_gray.shape[1]-1)*(0.5*(1 - master_slave))),
-                   int((image_gray.shape[1] - 1)*(1-master_slave*0.5))):
-        for x in range(image_gray.shape[0]-1):
+    for y in range(image.shape[1]-1):
+        for x in range(image.shape[0]-1):
             Sxx = np.sum(Ixx[x:x + 2, y:y + 2])
             Syy = np.sum(Iyy[x:x + 2, y:y + 2])
             Sxy = np.sum(Ixy[x:x + 2, y:y + 2])
             det = (Sxx * Syy) - (Sxy ** 2)
             trace = Sxx + Syy
-            r = det - k * (trace ** 2)
-            if r > 0:
+            min_eigenvalue = det/trace
+            # set the threshold for the minimum eigenvalue as high as 200 for detect only the sharpest corners
+            if min_eigenvalue > 300:
                 corners.append((x, y))
     return corners
 
@@ -39,9 +38,9 @@ def harris_corner_detection(image, master_slave):
 if __name__ == '__main__':
     img = cv2.imread('vanishing.png')
     cv2.imshow('original image', img)
-    corners = harris_corner_detection(img, 0)
+    corners = harris_corner_detection(img)
     for x, y in corners:
-        img[x, y] = (0, 0, 255)
+        img[x, y] = (0, 255, 0)
     cv2.imshow('corner detection', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
